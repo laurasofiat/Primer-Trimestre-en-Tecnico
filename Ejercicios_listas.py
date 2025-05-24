b117d928c4c2ab807094c7f00347780fad70057e
Laura Torres <torresmurillolaurasofia01@gmail.com>
2025-05-24 16:56:54 -0500

    primer commit

--- Ejercicios_listas.py
+++ Ejercicios_listas.py
@@ -23,7 +23,7 @@ print("El número mayor es:",{max(números)},"y el número nmenor es: ",{min(nú
 print("Ejercicio 4")
 list=["Mariana","Julia","Abigail","Salomé","Carol","Sara","Luisa"]
 print(list)
-list.insert(7,"JULIANA") 
+list.insert(7,"JULIANA") #holaaaaa 
 print(list)
 ll=len(list)
 print(ll,"Nombres")
