118992894ac05f262026df5f1a71de68142f7efa
Laura Torres <torresmurillolaurasofia01@gmail.com>
2025-05-31 16:38:44 -0500

    Archivo

---
+++ Condicionales.py
@@ -0,0 +1,12 @@
+edad=int(input("Ingrese su edad:"))
+if edad>=18:
+    print("Es mayor de edad")
+else: print("Usted es menor de edad")
+print("fin")
+print("---------------------------------------------")
+com=input("¿Usted come carne?")
+if com=="no":
+    print("Usted es vegetariano o vegano.")
+else: print("Usted no es vegano o vegetariano.")
+print("fin")
+print("---------------------------------------------")
